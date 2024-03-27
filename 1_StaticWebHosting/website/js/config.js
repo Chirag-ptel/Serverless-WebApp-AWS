bebf8361bb69ada16c3f7808e5ffb767883089f6
Chirag-ptel <chiragpatel140601@gmail.com>
2024-03-27 13:56:49 +0530

    ride.html updated

--- 1_StaticWebHosting/website/js/config.js
+++ 1_StaticWebHosting/website/js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_U7P67cVCd', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1ab2l6nrskkptbeep1ibr008uu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_TaBIiPQ82', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3npd232et9v6m65gnrtgq23mnu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
